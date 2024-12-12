82e881cd2c197c7b4807d330e739d08b7d963f77
kweku-xvi <nksarps@gmail.com>
2024-12-12 11:20:26 +0000

    added accounts to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
+from .models import User
 from django.contrib import admin
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'is_verified')
+    readonly_fields = ('created_at',)
+
+
+admin.site.register(User, UserAdmin)

c23cdefd0312af5cb8450378cb61be73e8df7fcb
kweku-xvi <nksarps@gmail.com>
2024-12-15 08:49:18 +0000

    added status field to book serializer

--- books/serializers.py
+++ books/serializers.py
@@ -5,6 +5,6 @@ from rest_framework import serializers
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['id', 'title', 'isbn', 'description', 'author', 'publication_date', 'genre', 'user', 'created_at']
+        fields = ['id', 'title', 'isbn', 'description', 'status', 'author', 'publication_date', 'genre', 'user', 'created_at']
 
-        read_only_fields = ['user']
+        read_only_fields = ['id', 'user']

a9d6f8396a1e23c12a56404bb1f51a5ca212392f
kweku-xvi <nksarps@gmail.com>
2024-12-15 08:48:50 +0000

    added reading status to books

--- books/models.py
+++ books/models.py
@@ -4,6 +4,12 @@ from django.db import models
 
 
 class Book(models.Model):
+    READING_STATUS = [
+        ('not_started', 'Not Started'),
+        ('reading', 'Reading'),
+        ('completed', 'Completed')
+    ]
+
     id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
     isbn = models.CharField(max_length=13, unique=True, blank=True, null=True)
     title = models.CharField(max_length=255)
@@ -11,6 +17,7 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     publication_date = models.DateField()
     genre = models.CharField(max_length=100)
+    status = models.CharField(max_length=50, choices=READING_STATUS, default='not_started')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 

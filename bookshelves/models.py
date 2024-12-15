3d065ba92e8e61e15cd3f091e0f03d74bc17b688
kweku-xvi <nksarps@gmail.com>
2024-12-15 10:44:30 +0000

    added bookshelf model

--- bookshelves/models.py
+++ bookshelves/models.py
@@ -1,3 +1,16 @@
+from accounts.models import User
 from django.db import models
 
-# Create your models here.
+
+class Bookshelf(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('-created_at',)

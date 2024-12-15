aa18f78a3524756a2c2c2353f7675aac309e7cc9
kweku-xvi <nksarps@gmail.com>
2024-12-15 10:44:15 +0000

    added bookshelf model to admin panel

--- bookshelves/admin.py
+++ bookshelves/admin.py
@@ -1,3 +1,10 @@
+from .models import Bookshelf
 from django.contrib import admin
 
-# Register your models here.
+
+class BookshelfAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'user', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+
+
+admin.site.register(Bookshelf, BookshelfAdmin)

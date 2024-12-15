88f58ec2cced17040a188a452a549fe422f8f474
kweku-xvi <nksarps@gmail.com>
2024-12-15 10:41:21 +0000

    added shelf to book serializer

--- books/serializers.py
+++ books/serializers.py
@@ -5,6 +5,6 @@ from rest_framework import serializers
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['id', 'title', 'isbn', 'description', 'status', 'author', 'publication_date', 'genre', 'user', 'created_at']
+        fields = ['id', 'title', 'isbn', 'description', 'status', 'author', 'publication_date', 'genre', 'shelf', 'user', 'shelf', 'created_at']
 
-        read_only_fields = ['id', 'user']
+        read_only_fields = ['id', 'user', 'shelf']

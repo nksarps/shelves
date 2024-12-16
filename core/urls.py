e7b86e5894248dae88823f2c7c59960abebfad03
kweku-xvi <nksarps@gmail.com>
2024-12-16 08:36:56 +0000

    added url endpoint for bookshelves

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('books/', include('books.urls')),
+    path('shelves/', include('bookshelves.urls')),
 ]

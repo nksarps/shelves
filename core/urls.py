7a6867218dc4c91450e9dd7c9de0c4a151024cad
kweku-xvi <nksarps@gmail.com>
2024-12-16 10:30:55 +0000

    added drf-yasg configuration to api documentation

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Shelves API",
+      default_version='v1',
+      description="Shelves is a REST API allowing users to manage their personal book collections.",
+      terms_of_service="https://github.com/kweku-xvi/shelves",
+      contact=openapi.Contact(email="nksarps@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('books/', include('books.urls')),
     path('shelves/', include('bookshelves.urls')),
+    path('swagger/json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]

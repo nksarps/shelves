9c05c81498b2740a712ec103b0cc8ba971530177
kweku-xvi <nksarps@gmail.com>
2024-12-16 08:35:37 +0000

    added bookshelf serializer

---
+++ bookshelves/serializers.py
@@ -0,0 +1,8 @@
+from .models import Bookshelf
+from rest_framework import serializers
+
+
+class BookshelfSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bookshelf
+        fields = ['id', 'name', 'description', 'user', 'created_at', 'updated_at']
